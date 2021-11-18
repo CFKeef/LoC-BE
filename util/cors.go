c39d7fe052c38c8e7c0285a25e54ef16bab29b5e
CFKeef <patryckg@outlook.com>
2021-11-17 20:31:22 -0500

    Just changing the origin allowed

--- util/cors.go
+++ util/cors.go
@@ -3,13 +3,12 @@ package util
 import "net/http"
 
 /*
-	Cors HAndler
+	Cors Handler
 	Sets up the headers for CORS and handles the options request
 */
 func CORS(next http.HandlerFunc) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		//w.Header().Add("Access-Control-Allow-Origin", "https://scala.patryck.dev")
-		w.Header().Add("Access-Control-Allow-Origin", "*")
+		w.Header().Add("Access-Control-Allow-Origin", "https://scala.patryck.dev")
 		w.Header().Add("Access-Control-Allow-Credentials", "true")
 		w.Header().Add("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With")
 		w.Header().Add("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
